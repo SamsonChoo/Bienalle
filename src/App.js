74f5d90026fe1410057fffeb77eec074f57fb277
SamsonChoo <hanye_choo@mymail.sutd.edu.sg>
2020-04-25 18:36:53 +0800

    Set up homepage

--- src/App.js
+++ src/App.js
@@ -1,24 +1,25 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="Header">
+        Future Hybrid Highrise Commune
+      </div>
+      <div className='SubHeader'>
+        How Will We Live Together
+      </div>
+      <div className='Content'>
+        <div className='TinyHeader'>
+          participate as prospective tenants
+        </div>
+        <div className='IntroText'>
+        In the launch of a high-rise commune, you will be able to express wishes regarding your own apartment, together with numerous other future neighbours. The wishes are based on a layout that allows numerous apartment sizes and life-styles to inhabit them. Having many parties involved though requires a moderation and negotiation process.
+Start your journey below!
+        </div>
+      </div>
+      <button class="button">Begin</button>
     </div>
   );
 }
